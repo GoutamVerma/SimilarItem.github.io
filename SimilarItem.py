90998b81a589317b81b77030b9a8f78111abcf85
Goutam Verma <66783850+GoutamVerma@users.noreply.github.com>
2020-06-23 20:53:49 +0530

    Create SimilarItem.py

---
+++ SimilarItem.py
@@ -0,0 +1,11 @@
+set1 = set(input("enter set elements"))
+set2 = set(input("enter set elements"))
+Comman = set()
+for a in set1:
+    for b in set2: 
+       if a==b: 
+         Comman.add(a)
+       
+       
+
+print(Comman)
